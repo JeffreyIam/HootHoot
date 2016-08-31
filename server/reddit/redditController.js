3d527a7ce089561d96b0291f3b94756881f381ae
Jeff Lam <jefflam1200@gmail.com>
2016-08-31 12:13:09 -0700

    (fix) add error handling to api requests

--- server/reddit/redditController.js
+++ server/reddit/redditController.js
@@ -3,10 +3,13 @@ var request = require('request');
 function reddit(req,res){
  request('https://www.reddit.com/r/' + req.body.subreddit + '/.json?limit=1', function(error, response, body) {
     var redditJson = JSON.parse(body);
+    if(redditJson.message === 'Not Found' || redditJson.data.children.length === 0) {
+      res.json({text: 'Reddit Query Not Found'});
+    } else {
     var title = redditJson.data.children[0].data.title;
     var url = redditJson.data.children[0].data.url;
-
     res.json({ text: title + ' ' + '[:link:]' + url + '[:link:]' });
+    }
   })
 }
 
--- server/stock/stockController.js
+++ server/stock/stockController.js
@@ -3,11 +3,15 @@ var request = require('request');
 function stock(req,res){
 
   request('http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + req.body.symbol + '&callback=myFunction', function(error, response, body) {
-
     var seperate = body.replace('})', '}').split('myFunction(');
     var result = JSON.parse(seperate[1]);
+    if(body.indexOf('No symbol matches') >= 0) {
+      res.json({text: 'No symbol found'});
+    } else {
     res.json({ text: req.body.symbol + ": " + 'Last Price: $' + result.LastPrice + ' ' +'Change: $' + result.Change.toString().slice(0,5) + ' ' + 'Open: $' + result.Open + ' ' + 'High: $' + result.High + ' ' + 'Low: $' + result.Low});
+    }
   })
 }
 
 module.exports = {stock};
+
